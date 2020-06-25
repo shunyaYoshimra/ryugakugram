bc2563fe64a0c71e8be7358551b1d4f661c39c2b
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-25 18:10:25 +0900

    tidy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
+  resources :users
+  get '/about', to: 'static_pages#about'
+  root to: 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
