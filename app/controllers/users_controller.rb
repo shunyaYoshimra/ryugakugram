d1af678fc477e80020bcea538bbc1a39d53432f0
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-25 18:37:47 +0900

    make user_create_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      flash[:notice] = "#{user.name}さん、ようこそRyugakugramへ！"
+      redirect_to user
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :passwrod_confirmation, :profile, :country)
   end
 end
