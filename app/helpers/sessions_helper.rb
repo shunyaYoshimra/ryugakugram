88b573cd12ac97157c84b78dc1b0f4fda49e1212
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-25 20:24:45 +0900

    improve current_user_function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+
+  def current_user
+    if(user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
