40b75807535fc045d9f86fd7fc3f4f8fe1cd95cc
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-26 10:18:15 +0900

    modify session function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
+      log_in user
       remember user
       flash[:notice] = "#{user.name}さん、ようこそRyugakugramへ！"
       redirect_to user
