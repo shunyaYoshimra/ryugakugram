b54be74a62da7213fef9a6cba071e1a35589b0b4
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-26 11:41:10 +0900

    make new_post page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :image
+  has_many :posts
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true , length: {maximum: 255}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
