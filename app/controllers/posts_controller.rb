5e44f0ed15d8164b0d8411f327bffc8e3d92c06b
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-26 12:32:33 +0900

    make post_new finction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,28 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
 
+  def create
+    post = current_user.posts.new(post_params)
+    if post.save
+      redirect_to post
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :picture)
   end
 end
