446e260db6d6d6c5ce9341eb82bee2138d4fd592
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-26 10:55:27 +0900

    modify user_image function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :passwrod_confirmation, :profile, :country)
+    params.require(:user).permit(:name, :email, :password, :passwrod_confirmation, :profile, :country, :image)
   end
 end
