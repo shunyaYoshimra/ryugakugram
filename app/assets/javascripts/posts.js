c58d4665955ad32c0ea84a073875742dce1198de
shunyaYoshimura <maoorgri1015@gmail.com>
2020-06-26 17:51:42 +0900

    add ajax function to post delete

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,19 @@
+document.addEventListener('turbolinks:load', function () {
+  document.querySelectorAll('.delete').forEach(function (a) {
+    a.addEventListener('ajax:success', function () {
+      var td = a.parentNode;
+      var tr = td.parentNode;
+      tr.style.display = 'none';
+    });
+  });
+});
+
+document.addEventListener('turbolinks:load', function () {
+  document.querySelectorAll('.comment_delete').forEach(function (a) {
+    a.addEventListener('ajax:success', function () {
+      var td = a.parentNode;
+      var tr = td.parentNode;
+      tr.style.display = 'none';
+    });
+  });
+});
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,12 +29,12 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_url
+    head :no_content
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:content, :picture)
+    params.permit(:content, :picture)
   end
 end
